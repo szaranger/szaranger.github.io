46037848318b89f1640b5d63cb9482c656120b70
Sean Amarasinghe <szaranger@yahoo.com>
2016-02-06 10:03:02 +1100

    added detail registration

---
+++ service-workers/01/02/service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(e) {
+  console.log('Install event:', e);
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('Activate event:', e);
+});
