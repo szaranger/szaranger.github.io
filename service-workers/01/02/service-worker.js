1fd12b4596f7ae18f0f7409bebae777cbf350c99
Sean Amarasinghe <szaranger@yahoo.com>
2016-02-06 10:48:46 +1100

    removed wait

--- service-workers/01/02/service-worker.js
+++ service-workers/01/02/service-worker.js
@@ -2,9 +2,6 @@ self.addEventListener('install', function(e) {
   console.log('Install Event:', e);
 });
 
-self.addEventListener('wait', function(e) {
-  console.log('Wait Event:', e);
-});
 
 self.addEventListener('activate', function(e) {
   console.log('Activate Event:', e);
