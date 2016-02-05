02468d2c704659a066af84a6f0be465ee84efb2b
Sean Amarasinghe <szaranger@yahoo.com>
2016-02-06 10:46:33 +1100

    Added helper function

--- service-workers/01/02/service-worker.js
+++ service-workers/01/02/service-worker.js
@@ -1,7 +1,11 @@
 self.addEventListener('install', function(e) {
-  console.log('Install event:', e);
+  console.log('Install Event:', e);
+});
+
+self.addEventListener('wait', function(e) {
+  console.log('Wait Event:', e);
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('Activate event:', e);
+  console.log('Activate Event:', e);
 });
