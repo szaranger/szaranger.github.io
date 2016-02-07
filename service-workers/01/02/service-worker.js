643e76397ec53230207be63af282fcd8ee14b843
Sean Amarasinghe <szaranger@yahoo.com>
2016-02-07 13:04:45 +1100

    added mock resposnes

--- service-workers/01/02/service-worker.js
+++ service-workers/01/02/service-worker.js
@@ -2,7 +2,6 @@ self.addEventListener('install', function(e) {
   console.log('Install Event:', e);
 });
 
-
 self.addEventListener('activate', function(e) {
   console.log('Activate Event:', e);
 });
---
+++ service-workers/01/03/service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('fetch', function(e) {
+  console.log('Handling fetch event for', event.request.url);
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.pathname === '/urlshortener/v1/url' &&
+      event.request.headers.has('X-Mock-Response')) {
+
+    var responseBody = {
+      kind: 'urlshortener#url',
+      id: 'http://goo.gl/IKyjuU',
+      longUrl: 'https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html'
+    };
+
+    var responseInit = {
+
+      status: 200,
+      statusText: 'OK',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Mock-Response': 'yes'
+      }
+    };
+
+    var mockResponse = new Response(JSON.stringify(responseBody), responseInit);
+
+    console.log('Responding with a mock response body:', responseBody);
+    event.respondWith(mockResponse);
+  }
+});
