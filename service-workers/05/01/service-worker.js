a1ef400f93eb6ba018212a2ab705b177ed98d512
Sean Saranga Amarasinghe <szaranger@yahoo.com>
2016-04-28 23:17:23 +1000

    Update service-worker.js

--- service-workers/05/01/service-worker.js
+++ service-workers/05/01/service-worker.js
@@ -1,43 +1,17 @@
-'use strict';
+var version = 1;
+var cacheName = 'responses-offline' + version;
 
-importScripts('./vendor/zip.js');
-importScripts('./vendor/ArrayBufferReader.js');
-importScripts('./vendor/deflate.js');
-importScripts('./vendor/inflate.js');
-importScripts('./helper.js');
-importScripts('./cacheProvider.js');
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+});
 
-var zipURL = './archive.zip',
-  cachePromise,
-  contentTypes = {
-    css : 'text/css',
-    js : 'application/javascript',
-    png : 'image/png',
-    jpg : 'image/jpeg',
-    jpeg: 'image/jpeg',
-    html: 'text/html',
-    htm : 'text/html'
-  };
+self.addEventListener('activate', function(event) {
+    if (self.clients && clients.claim) {
+        clients.claim();
+    }
+});
 
-zip.useWebWorkers = false;
-
-self.oninstall = function(event) {
-  event.waitUntil(
-    fetch(zipURL)
-      .then(function(res) {
-        return res.arrayBuffer();
-      })
-      .then(getZipFileReader)
-      .then(cacheFileContents)
-      .then(self.skipWaiting.bind(self))
-  );
-};
-
-self.onactivate = function(evt) {
-  evt.waitUntil(self.clients.claim());
-};
-
-self.onfetch = function(evt) {
+self.addEventListener('fetch', function(evt) {
   evt.respondWith(openCache().then(function(cache) {
     var request = evt.request;
 
@@ -45,4 +19,4 @@ self.onfetch = function(evt) {
       return res || fetch(request);
     });
   }));
-};
+});
