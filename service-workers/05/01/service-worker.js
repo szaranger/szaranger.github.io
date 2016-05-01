45dccf998d107e9614ce60751a8aafc4da348353
Sean Saranga Amarasinghe <szaranger@yahoo.com>
2016-05-01 11:41:25 +1000

    Update service-worker.js

--- service-workers/05/01/service-worker.js
+++ service-workers/05/01/service-worker.js
@@ -1,22 +1,39 @@
-var version = 1;
-var cacheName = 'responses-offline' + version;
+'use strict';
+
+var cacheName= 'resource-from-network';
 
 self.addEventListener('install', function(event) {
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        return cache.addAll([
+          'index.html',
+          'index.js'
+        ]);
+      })
+      .then(function() {
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          console.log('Fetching from the cache: ', event.request.url);
+          return response;
+        } else {
+          console.log('Fetching from server: ', event.request.url);
+        }
+       return fetch(event.request);
+     }
+   )
+ );
 });
 
 self.addEventListener('activate', function(event) {
-    if (self.clients && clients.claim) {
-        clients.claim();
-    }
-});
-
-self.addEventListener('fetch', function(evt) {
-  evt.respondWith(openCache().then(function(cache) {
-    var request = evt.request;
-
-    return cache.match(request).then(function(res) {
-      return res || fetch(request);
-    });
-  }));
+   console.log('Activating the service worker!');
+   event.waitUntil(self.clients.claim());
 });
