423a38cbfa51b953ab087136666ad4188e5308b1
Sean Saranga Amarasinghe <szaranger@yahoo.com>
2016-05-14 19:39:28 +1000

    Update service-worker.js

--- service-workers/05/08/service-worker.js
+++ service-workers/05/08/service-worker.js
@@ -17,6 +17,33 @@ self.addEventListener('install', function(evt) {
   );
 });
 
+self.addEventListener('activate', function(evt) {
+  self.clients.matchAll({
+    includeUncontrolled: true
+  }).then(function(clientList) {
+    var urls = clientList.map(function(client) {
+      return client.url;
+    });
+    console.log('SERVICE_WORKER:  Matching clients:', urls.join(', '));
+  });
+
+  evt.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== VERSION) {
+            console.log('SERVICE_WORKER:  Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(function() {
+      console.log('SERVICE_WORKER:  Claiming clients for version', VERSION);
+      return self.clients.claim();
+    })
+  );
+});
+
 self.addEventListener('fetch', function(evt) {
   if (evt.request.url.includes('/picture.jpg')) {
     console.log('SERVICE_WORKER: Serving picture.jpg for', evt.request.url);
