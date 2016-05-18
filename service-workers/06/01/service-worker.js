4d319c5f6602a1cbd9f1209df18b4fcdebab0063
Sean Saranga Amarasinghe <szaranger@yahoo.com>
2016-05-18 20:50:43 +1000

    Add files via upload

---
+++ service-workers/06/01/service-worker.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log(
+  'Cache', this.Cache, '\n',
+  'caches', this.caches,'\n',
+  'fetch', this.fetch,'\n',
+  'getAll', this.getAll,'\n',
+  'Request', this.Request,'\n',
+  'Response', this.Response
+);
