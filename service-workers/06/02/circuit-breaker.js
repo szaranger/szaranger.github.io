e8ad9adaecdc889753ab7245f4d067e6d7b9d235
Sean Saranga Amarasinghe <szaranger@yahoo.com>
2016-05-21 11:14:05 +1000

    Update circuit-breaker.js

--- service-workers/06/02/circuit-breaker.js
+++ service-workers/06/02/circuit-breaker.js
@@ -1,184 +1,191 @@
-var TEN_SECONDS = 10000,
-  TEN_BUCKETS = 10,
-  THREE_SECONDS = 3000,
-  FIFTY_PERCENT = 50,
-  FIVE = 5;
+var CircuitBreaker = (function() {
+  'use strict';
 
-var CB = function(opts) {
-  opts = opts || {};
+  var TEN_SECONDS = 10000,
+    TEN_BUCKETS = 10,
+    THREE_SECONDS = 3000,
+    FIFTY_PERCENT = 50,
+    FIVE = 5;
 
-  this.errorThreshold  = opts.errorThreshold || FIFTY_PERCENT;
-  this.numBuckets = opts.numBuckets || TEN_BUCKETS;
-  this.timeoutDuration = opts.timeoutDuration || THREE_SECONDS;
-  this.volumeThreshold = opts.volumeThreshold || FIVE;
-  this.windowDuration  = opts.windowDuration || TEN_SECONDS;
+  var CB = function(opts) {
+    opts = opts || {};
 
-  this.onCircuitOpen   = opts.onCircuitOpen  || function() {};
-  this.onCircuitClose  = opts.onCircuitClose || function() {};
+    this.errorThreshold  = opts.errorThreshold || FIFTY_PERCENT;
+    this.numBuckets = opts.numBuckets || TEN_BUCKETS;
+    this.timeoutDuration = opts.timeoutDuration || THREE_SECONDS;
+    this.volumeThreshold = opts.volumeThreshold || FIVE;
+    this.windowDuration  = opts.windowDuration || TEN_SECONDS;
 
-  this.$buckets = [this.$createBucket()];
-  this.$state = CB.CLOSED;
+    this.hanldeCircuitOpen   = opts.hanldeCircuitOpen  || function() {};
+    this.handleCircuitClose  = opts.handleCircuitClose || function() {};
 
-  this.$startTicker();
-};
+    this.$buckets = [this.$createBucket()];
+    this.$state = CB.CLOSED;
 
-CB.OPEN = 0;
-CB.HALF_OPEN = 1;
-CB.CLOSED = 2;
-
-CB.prototype.run = function(command, fallback) {
-  if (this.isOpen()) {
-    this.$executeFallback(fallback || function() {});
-  } else {
-    this.$execCmd(command);
-  }
-};
-
-CB.prototype.forceClose = function() {
-  this.$forced = this.$state;
-  this.$state = CB.CLOSED;
-};
-
-CB.prototype.forceOpen = function() {
-  this.$forced = this.$state;
-  this.$state = CB.OPEN;
-};
-
-CB.prototype.unforce = function() {
-  this.$state = this.$forced;
-  this.$forced = null;
-};
-
-CB.prototype.isOpen = function() {
-  return this.$state == CB.OPEN;
-};
-
-CB.prototype.$startTicker = function() {
-  var me = this,
-    bucketIndex = 0,
-    bucketDuration = this.windowDuration / this.numBuckets;
-
-  var tick = function() {
-    if (me.$buckets.length > me.numBuckets) {
-      me.$buckets.shift();
-    }
-
-    bucketIndex++;
-
-    if (bucketIndex > me.numBuckets) {
-      bucketIndex = 0;
-
-      if (me.isOpen()) {
-        me.$state = CB.HALF_OPEN;
-      }
-    }
-
-    me.$buckets.push(me.$createBucket());
+    this.$startTicker();
   };
 
-  setInterval(tick, bucketDuration);
-};
+  CB.OPEN = 0;
+  CB.HALF_OPEN = 1;
+  CB.CLOSED = 2;
 
-CB.prototype.$createBucket = function() {
-  return {
-    successes: 0,
-    failures: 0,
-    shortCircuits: 0,
-    timeouts: 0
+  CB.prototype.run = function(command, fallback) {
+    if (this.isOpen()) {
+      this.$executeFallback(fallback || function() {});
+    } else {
+      this.$execCmd(command);
+    }
   };
-};
 
-CB.prototype.$lastBucket = function() {
-  var numBuckets = this.$buckets.length,
-    lastBucket = this.$buckets[numBuckets - 1];
-    
-  return lastBucket;
-};
+  CB.prototype.forceClose = function() {
+    this.$forced = this.$state;
+    this.$state = CB.CLOSED;
+  };
 
-CB.prototype.$execCmd = function(command) {
-  var me = this,
-    timeout;
+  CB.prototype.forceOpen = function() {
+    this.$forced = this.$state;
+    this.$state = CB.OPEN;
+  };
 
-  var increment = function(prop) {
-    return function() {
-      var bucket;
+  CB.prototype.unforce = function() {
+    this.$state = this.$forced;
+    this.$forced = null;
+  };
 
-      if (!timeout) {
-        return;
+  CB.prototype.isOpen = function() {
+    return this.$state === CB.OPEN;
+  };
+
+  CB.prototype.$startTicker = function() {
+    var me = this,
+      bucketIndex = 0,
+      bucketDuration = this.windowDuration / this.numBuckets;
+
+    var tick = function() {
+      if (me.$buckets.length > me.numBuckets) {
+        me.$buckets.shift();
       }
 
-      bucket = me.$lastBucket();
-      bucket[prop]++;
+      bucketIndex++;
 
-      if (me.$forced === null) {
-        me.$updateState();
+      if (bucketIndex > me.numBuckets) {
+        bucketIndex = 0;
+
+        if (me.isOpen()) {
+          me.$state = CB.HALF_OPEN;
+        }
       }
 
-      clearTimeout(timeout);
-      timeout = null;
+      me.$buckets.push(me.$createBucket());
+    };
+
+    setInterval(tick, bucketDuration);
+  };
+
+  CB.prototype.$createBucket = function() {
+    return {
+      successes: 0,
+      failures: 0,
+      shortCircuits: 0,
+      timeouts: 0
     };
   };
 
-  timeout = setTimeout(increment('timeouts'), this.timeoutDuration);
+  CB.prototype.$lastBucket = function() {
+    var numBuckets = this.$buckets.length,
+      lastBucket = this.$buckets[numBuckets - 1];
 
-  command(increment('successes'), increment('failures'));
-};
-
-CB.prototype.$executeFallback = function(fallback) {
-  var bucket;
-
-  fallback();
-
-  bucket = this.$lastBucket();
-  bucket.shortCircuits++;
-};
-
-CB.prototype.$calcMetrics = function() {
-  var totalCount = 0,
-    totalErrors = 0,
-    errorPerc = 0,
-    bucket,
-    errors,
-    i;
-
-  for (i = 0, len = this.$buckets.length; i < len; i++) {
-    bucket = this.$buckets[i];
-    errors = (bucket.failures + bucket.timeouts);
-
-    totalErrors += errors;
-    totalCount += (errors + bucket.successes);
-  }
-
-  errorPerc = (totalErrors / (totalCount > 0 ? totalCount : 1)) * 100;
-
-  return {
-    totalErrors: totalErrors,
-    errorPerc: errorPerc,
-    totalCount: totalCount
+    return lastBucket;
   };
-};
 
-CB.prototype.$updateState = function() {
-  var metrics = this.$calcMetrics();
+  CB.prototype.$execCmd = function(command) {
+    var me = this,
+      increment,
+      timeout;
 
-  if (this.$state == CB.HALF_OPEN) {
-    var lastCmdFailed = !this.$lastBucket().successes && metrics.totalErrors > 0;
+    increment = function(prop) {
+      return function() {
+        var bucket;
 
-    if (lastCmdFailed) {
-      this.$state = CB.OPEN;
-    } else {
-      this.$state = CB.CLOSED;
-      this.onCircuitClose(metrics);
+        if (!timeout) {
+          return;
+        }
+
+        bucket = me.$lastBucket();
+        bucket[prop]++;
+
+        if (me.$forced === null) {
+          me.$updateState();
+        }
+
+        clearTimeout(timeout);
+        timeout = null;
+      };
+    };
+
+    timeout = setTimeout(increment('timeouts'), this.timeoutDuration);
+
+    command(increment('successes'), increment('failures'));
+  };
+
+  CB.prototype.$executeFallback = function(fallback) {
+    var bucket;
+
+    fallback();
+
+    bucket = this.$lastBucket();
+    bucket.shortCircuits++;
+  };
+
+  CB.prototype.$calcMetrics = function() {
+    var totalCount = 0,
+      totalErrors = 0,
+      errorPerc = 0,
+      bucket,
+      errors,
+      i;
+
+    for (i = 0, len = this.$buckets.length; i < len; i++) {
+      bucket = this.$buckets[i];
+      errors = (bucket.failures + bucket.timeouts);
+
+      totalErrors += errors;
+      totalCount += (errors + bucket.successes);
     }
-  }
-  else {
-    var overErrorThreshold = metrics.errorPerc > this.errorThreshold,
-      overVolumeThreshold = metrics.totalCount > this.volumeThreshold,
-      overThreshold = overVolumeThreshold && overErrorThreshold;
 
-    if (overThreshold) {
-      this.$state = CB.OPEN;
-      this.onCircuitOpen(metrics);
+    errorPerc = (totalErrors / (totalCount > 0 ? totalCount : 1)) * 100;
+
+    return {
+      totalErrors: totalErrors,
+      errorPerc: errorPerc,
+      totalCount: totalCount
+    };
+  };
+
+  CB.prototype.$updateState = function() {
+    var metrics = this.$calcMetrics();
+
+    if (this.$state == CB.HALF_OPEN) {
+      var lastCmdFailed = !this.$lastBucket().successes && metrics.totalErrors > 0;
+
+      if (lastCmdFailed) {
+        this.$state = CB.OPEN;
+      } else {
+        this.$state = CB.CLOSED;
+        this.handleCircuitClose(metrics);
+      }
     }
-  }
-};
+    else {
+      var overErrorThreshold = metrics.errorPerc > this.errorThreshold,
+        overVolumeThreshold = metrics.totalCount > this.volumeThreshold,
+        overThreshold = overVolumeThreshold && overErrorThreshold;
+
+      if (overThreshold) {
+        this.$state = CB.OPEN;
+        this.hanldeCircuitOpen(metrics);
+      }
+    }
+  };
+
+  return CB;
+})();
