0c943fa0ec2d45a048b7f0120565ec46a41ad999
Sean Saranga Amarasinghe <szaranger@yahoo.com>
2016-06-13 17:50:49 +1000

    Delete service-worker.js

--- service-workers/01/02/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(e) {
-  console.log('Install Event:', e);
-});
-
-self.addEventListener('activate', function(e) {
-  console.log('Activate Event:', e);
-});
