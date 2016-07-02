503554eb6da2ade86dbbbec2f21bd219c40957cd
Sean Saranga Amarasinghe <szaranger@yahoo.com>
2016-07-02 19:53:27 +1000

    Update server.js

--- service-workers/08/06/server.js
+++ service-workers/08/06/server.js
@@ -1,69 +1,122 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var https = require('https');
-var fs = require('fs');
-var webPush = require('web-push');
+'use strict';
 
-var privateKey  = fs.readFileSync('/private/etc/apache2/localhost-key.pem', 'utf8');
-var certificate = fs.readFileSync('/private/etc/apache2/localhost-cert.pem', 'utf8');
-var credentials = { key: privateKey, cert: certificate };
-var httpsServer = https.createServer(credentials, app);
-var port = 3012;
+var scope = {
+  scope: './'
+};
 
-var subscriptions = [];
-var pushInterval = 10;
+var endpoint;
+var baseURL = 'https://localhost:3012/';
+var subscriptionBtn = document.querySelector('#subscription-button');
 
-webPush.setGCMAPIKey('AIzaSyCC3YXfz4w2j3-HcXvwBQ0B_JddR95d3wo'/*process.env.GCM_API_KEY*/);
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use(bodyParser.json());
-
-app.get('/', function (req, res) {
-  res.send('Hello service worker!');
-});
-
-setInterval(function() {
-  subscriptions.forEach(sendNotification);
-}, pushInterval * 1000);
-
-function isSubscribed(endpoint) {
-  return (subscriptions.indexOf(endpoint) >= 0);
-}
-
-function sendNotification(endpoint) {
-  webPush.sendNotification(endpoint).then(function() {
-    console.log('Push Application Server - Notification sent to ' + endpoint);
-  }).catch(function() {
-    console.log('ERROR in sending Notification, endpoint removed ' + endpoint);
-    subscriptions.splice(subscriptions.indexOf(endpoint), 1);
-  });
-}
-
-app.post('/register', function(req, res) {
-  var endpoint = req.body.endpoint;
-   if (!isSubscribed(endpoint)) {
-     console.log('Subscription registered ' + endpoint);
-     subscriptions.push(endpoint);
-   }
-   res.type('js').send('{"success":true}');
-});
-
-app.post('/unregister', function(req, res) {
-  var endpoint = req.body.endpoint;
-    if (isSubscribed(endpoint)) {
-      console.log('Subscription unregistered ' + endpoint);
-      subscriptions.splice(subscriptions.indexOf(endpoint), 1);
+navigator.serviceWorker.register('service-worker.js')
+.then(function(registration) {
+  return registration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription;
     }
-    res.type('js').send('{"success":true}');
+
+    return registration.pushManager.subscribe({ userVisibleOnly: true });
+  });
+}).then(function(subscription) {
+  endpoint = subscription.endpoint;
+
+  fetch(baseURL + 'register', {
+    method: 'post',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      endpoint: subscription.endpoint,
+    }),
+  });
 });
 
-httpsServer.listen(port, function () {
-  console.log('App listening on port ' + port);
-});
+function printStatus(status) {
+  document.querySelector('#status').innerHTML = status;
+}
+
+document.querySelector('#resetButton').addEventListener('click',
+  function() {
+    navigator.serviceWorker.getRegistration().then(function(registration) {
+      registration.unregister();
+      window.location.reload();
+    });
+  }
+);
+
+function getSubscription() {
+  return navigator.serviceWorker.ready
+    .then(function(registration) {
+      return registration.pushManager.getSubscription();
+    });
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(function() {
+      console.log('service worker registered');
+      subscriptionBtn.removeAttribute('disabled');
+    });
+  getSubscription()
+    .then(function(subscription) {
+      if (subscription) {
+        console.log('Already subscribed', subscription.endpoint);
+        setUnsubscribeButton();
+      } else {
+        setSubscribeButton();
+      }
+    }
+  );
+}
+
+document.querySelector('#subscription-button').onclick = function() {
+  fetch(baseURL + 'sendNotification?endpoint=' + endpoint, {
+      method: 'post',
+  });
+};
+
+function subscribe() {
+  navigator.serviceWorker.ready.then(function(registration) {
+    return registration.pushManager.subscribe({ userVisibleOnly: true });
+  }).then(function(subscription) {
+    console.log('Subscribed', subscription.endpoint);
+    return fetch(baseURL + 'register', {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        endpoint: subscription.endpoint
+      })
+    });
+  }).then(setUnsubscribeButton);
+}
+
+function unsubscribe() {
+  getSubscription().then(function(subscription) {
+    return subscription.unsubscribe()
+      .then(function() {
+        console.log('Unsubscribed', subscription.endpoint);
+        return fetch(baseURL + 'unregister', {
+          method: 'post',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            endpoint: subscription.endpoint
+          })
+        });
+      });
+  }).then(setSubscribeButton);
+}
+
+function setSubscribeButton() {
+  subscriptionBtn.onclick = subscribe;
+  subscriptionBtn.textContent = 'Subscribe!';
+}
+
+function setUnsubscribeButton() {
+  subscriptionBtn.onclick = unsubscribe;
+  subscriptionBtn.textContent = 'Unsubscribe!';
+}
